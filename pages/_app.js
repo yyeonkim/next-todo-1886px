4fa80b03209e97018c205e4700a98cda70db6f2d
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-30 12:10:26 +0900

    Use chakra ui for style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
 
+import { extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const theme = extendTheme({});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
